ab93c6c15ad8c6ece4c3574ade1ef623064f1c09
André Luis dos Santos Sernajoto <69870279+andresernajoto@users.noreply.github.com>
2021-11-13 00:17:58 -0300

    exercises with classes
    Exercise about a product stock solved using methods in a class, to simplify the program.

---
+++ Estudos/Estoque/Estoque/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace Estoque {
+    class Program {
+        static void Main(string[] args) {
+
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Preço: $ ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+            Console.Write("Digite a quantidade de produtos a serem adicionados ao estoque: ");
+            int qtd = int.Parse(Console.ReadLine());
+            /* A linha abaixo faz a chamada do método AdicionarProdutos
+            recebendo como parâmetro a variável qtd, que informa a
+            quantidade adicionada + a quantidade já existente do estoque */
+            p.AdicionarProdutos(qtd);
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto atualizado: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite a quantidade de produtos a serem removidos do estoque: ");
+            /* Na linha abaixo a variável é reutilizada pois
+            já continha o valor anterior do estoque */
+            qtd = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qtd);
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto atualizado: " + p);
+
+        }
+    }
+}
