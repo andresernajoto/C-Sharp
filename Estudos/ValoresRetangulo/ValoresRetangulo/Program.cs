30ae6718eab9f81ac77c02fb2df8d8861f13e7d3
André Luis dos Santos Sernajoto <69870279+andresernajoto@users.noreply.github.com>
2021-11-13 23:47:57 -0300

    exercises with classes
    Solved exercises using classes to keep the program more clean and easier to understand.

---
+++ Estudos/ValoresRetangulo/ValoresRetangulo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace ValoresRetangulo {
+    class Program {
+        static void Main(string[] args) {
+
+            // instanciação da classe
+            Retangulo ret = new Retangulo();
+
+            // leitura da altura e largura do retângulo
+            Console.WriteLine("Entre com a largura e a altura do retângulo: ");
+            ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            // saída exibida na tela
+            Console.WriteLine("Área = " + ret.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perímetro = " + ret.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal = " + ret.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
