ff3ee6c342fb4e50bdfae7b0b80917af8a9ecd96
André Luis dos Santos Sernajoto <69870279+andresernajoto@users.noreply.github.com>
2021-11-22 23:33:38 -0300

    exercício de parâmetro
    Dois exemplos resolvidos usando parâmetros da linguagem C#, out e ref.

---
+++ Estudos/Ref/Ref/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ref {
+    class Program {
+        static void Main(string[] args) {
+
+            int a = 10;
+
+            Calculator.Triple(ref a);
+            Console.WriteLine(a);
+        }
+    }
+}
