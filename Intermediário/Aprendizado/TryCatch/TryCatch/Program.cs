27c5c1cd65fc830820bd3c03e26917e1376372f2
André Luis dos Santos Sernajoto <andre.sernajoto@gmail.com>
2021-12-11 20:40:14 -0300

    Conteúdo adicionado
    Conteúdo sobre a estrutura try-catch aprendido.

---
+++ Intermediário/Aprendizado/TryCatch/TryCatch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TryCatch {
+    class Program {
+        static void Main(string[] args) {
+
+            // o bloco try tentará rodar o programa
+            try {
+                int num1 = int.Parse(Console.ReadLine());
+                int num2 = int.Parse(Console.ReadLine());
+
+                int result = num1 / num2;
+
+                Console.WriteLine(result);
+                // se houver exceções, os blocos catchs abaixo irão entrar em cena
+            } catch (DivideByZeroException) {
+                Console.WriteLine();
+                Console.WriteLine("Division to zero is not allowed!");
+                // o 'e' abaixo é um apelido da exceção para chamar seus métodos
+            }
+            catch (FormatException e) {
+                Console.WriteLine();
+                Console.WriteLine("Format error! {0}", e.Message);
+            }
+        }
+    }
+}
