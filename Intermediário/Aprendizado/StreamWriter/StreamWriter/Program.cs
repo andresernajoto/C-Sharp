59d5f0cc17fc77672da90954cd2160b55ae24390
André Luis dos Santos Sernajoto <69870279+andresernajoto@users.noreply.github.com>
2022-01-10 20:54:06 -0300

    Conteúdo adicionado
    Conteúdo aprendido sobre StreamWriter, onde foi feita a utilização da classe para escrever num segundo arquivo, o mesmo conteúdo que havia no arquivo 1, porém com todas as suas letras maiúsculas.

---
+++ Intermediário/Aprendizado/StreamWriter/StreamWriter/Program.cs
@@ -0,0 +1,15 @@
+﻿string sourcePath = @"D:\.Downloads\Pessoal\Estudos\Programming\C#\Exercícios\StreamWriter\file1.txt";
+string targetPath = @"D:\.Downloads\Pessoal\Estudos\Programming\C#\Exercícios\StreamWriter\file2.txt";
+
+try {
+    string[] lines = File.ReadAllLines(sourcePath);
+
+    using (StreamWriter sw = File.AppendText(targetPath)) {
+        foreach (string line in lines) {
+            sw.WriteLine(line.ToUpper());
+        }
+    }
+} catch (IOException e) {
+    Console.WriteLine("An error occurred");
+    Console.WriteLine(e.Message);
+}
