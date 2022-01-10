5511d5c042ed6ae4460307c952325af9518e866b
André Luis dos Santos Sernajoto <69870279+andresernajoto@users.noreply.github.com>
2022-01-10 20:33:53 -0300

    Conteudo adicionado
    Conteúdo aprendido sobre o bloco using, que permite resumir o que foi construido dentro do arquivo WorkingWithFiles V2.0

---
+++ Intermediário/Aprendizado/UsingBlock/UsingBlock/Program.cs
@@ -0,0 +1,15 @@
+﻿string path = @"D:\.Downloads\Pessoal\Estudos\Programming\C#\Exercícios\UsingBlock\file1.txt";
+
+// método resumido de abrir um arquivo e imprimir suas linhas
+try {
+    using (StreamReader sr = File.OpenText(path)) {
+        while (!sr.EndOfStream) {
+            string line = sr.ReadLine();
+            Console.WriteLine(line);
+        }
+    }
+}
+catch (IOException e) {
+    Console.WriteLine("An error occurred");
+    Console.WriteLine(e.Message);
+}
