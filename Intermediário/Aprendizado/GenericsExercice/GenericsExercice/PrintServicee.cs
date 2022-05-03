7cd3d4618a2df3bc8c5205a0c18b016a29d7f42e
André Luis dos Santos Sernajoto <69870279+andresernajoto@users.noreply.github.com>
2022-05-02 22:27:55 -0300

    Conteúdo adicionado
    Primeira parte implementando o uso das coleções de Generics

---
+++ Intermediário/Aprendizado/GenericsExercice/GenericsExercice/PrintServicee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsExercice {
+    class PrintServicee {
+
+        // variáveis internas
+        private int[] _values = new int[10];
+        private int _count = 0;
+
+        // operação que adiciona um valor à classe printService
+        public void AddValue(int value) {
+            if (_count == 10) {
+                throw new InvalidOperationException("PrintService is full");
+            }
+            
+            _values[_count] = value;
+            _count++;
+        }
+
+        // retorna o primeiro valor que foi informado
+        public int First() {
+            if (_count == 0) {
+                throw new InvalidOperationException("PrintService is empty");
+            }
+            return _values[0];
+        }
+
+        // função que imprime os valores de maneira formatada
+        public void Print() {
+            Console.Write("[ ");
+            
+            for (int i = 0; i < _count - 1; i++) {
+                Console.Write(_values[i] + ", ");
+            }
+
+            if (_count > 0) {
+                Console.Write(_values[_count - 1]);
+            }
+            Console.Write(" ]");
+        }
+    }
+}
---
+++ Intermediário/Aprendizado/GenericsExercice/GenericsExercice/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GenericsExercice {
+    class Program {
+        static void Main(string[] args) {
+
+            // instanciação da classe PrintServicee
+            PrintServicee printService = new PrintServicee();
+
+            // leitura de quantos valores serão inseridos
+            Console.Write("How many values: ");
+            int num = int.Parse(Console.ReadLine());
+
+            // loop que lê os valores e armazena na operação AddValue()
+            for (int i = 0; i < num; i++) {
+                int values = int.Parse(Console.ReadLine());
+                printService.AddValue(values);
+            }
+
+            // apresentação dos dados de maneira formatada
+            Console.WriteLine();
+            printService.Print();
+            Console.WriteLine();
+            Console.WriteLine("First: " + printService.First());
+        }
+    }
+}
