dabb377952f1b686eaa652e8112b94b5d0e8a45e
André Luis dos Santos Sernajoto <69870279+andresernajoto@users.noreply.github.com>
2022-05-02 22:27:12 -0300

    Contéudo adicionado
    Implementada a solução com uso da interface IComparable, comparando os nomes e salário de um funcionário, para que possam ser organizados.

---
+++ Intermediário/Aprendizado/InterfaceIComparable2/InterfaceIComparable2/Entities/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace InterfaceIComparable2.Entities {
+    class Employee : IComparable {
+
+        // declaração dos atributos
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        // instanciação do CultureInfo para encurtar sua chamada
+        CultureInfo CI = CultureInfo.InvariantCulture;
+
+        // construtor em formato de vetor
+        // irá armazenar o nome na posiçãio 0 e salário na posição 1
+        public Employee(string csvEmployee) {
+            string[] vect = csvEmployee.Split(',');
+            Name = vect[0];
+            Salary = double.Parse(vect[1], CI);
+        }
+
+        // override do ToString() para o que será impresso na tela
+        public override string ToString() {
+            return Name
+                + ", "
+                + Salary.ToString("F2", CI);
+        }
+
+        // faz a comparação entre os nomes dos funcionários
+        public int CompareTo(object obj) {
+            if (!(obj is Employee)) {
+                throw new ArgumentException("Comparing error: argument is not an Employee");
+            }
+
+            Employee other = obj as Employee;
+            return Name.CompareTo(other.Name);
+        }
+    }
+}
---
+++ Intermediário/Aprendizado/InterfaceIComparable2/InterfaceIComparable2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using InterfaceIComparable2.Entities;
+
+namespace InterfaceIComparable2 {
+    class Program {
+        static void Main(string[] args) {
+
+            string path = @"D:\.Downloads\Andre\Estudos\Programming\C#\Exercícios\InterfaceIComparable2\in.txt";
+
+            /* o bloco try a seguir irá ler o caminho do arquivo
+             em seguida criará uma lista aonde serão adicionados
+            os nomes da pessoas até que a lista chegue ao final */
+            try {
+                using (StreamReader sr = File.OpenText(path)) {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream) {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+
+                    // faz a organização dos dados de uma lista
+                    list.Sort();
+                    foreach (Employee emp in list) {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error ocurred: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
