ced947db3f993a150704805373cbc37f6ad66189
André Luis dos Santos Sernajoto <69870279+andresernajoto@users.noreply.github.com>
2022-05-05 18:30:04 -0300

    Conteúdo adicionado
    Exemplo simples das operações Equals e GetHashCode com uma classe personalizada.

---
+++ Intermediário/Aprendizado/TestingGetHashCode-Equals/TestingGetHashCode-Equals/Entities/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingGetHashCode_Equals.Entities {
+    class Client {
+
+        // atributos da classe
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        // override de Equals para saber se os emails são iguais
+        public override bool Equals(object obj) {
+            // caso obj não seja do tipo Client,  retornar falso
+            if (!(obj is Client)) {
+                return false;
+            }
+
+            // downcasting
+            Client other = obj as Client;
+            return Email.Equals(other.Email);
+        }
+
+        // override do GetHashCode para comparar o Email
+        public override int GetHashCode() {
+            return Email.GetHashCode();
+        }
+    }
+}
---
+++ Intermediário/Aprendizado/TestingGetHashCode-Equals/TestingGetHashCode-Equals/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TestingGetHashCode_Equals.Entities;
+
+namespace TestingGetHashCode_Equals {
+    class Program {
+        static void Main(string[] args) {
+
+            // instanciação da classe Client com os valores
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "alex@gmail.com" };
+
+            // impressão do Equals e GetHashCode
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
