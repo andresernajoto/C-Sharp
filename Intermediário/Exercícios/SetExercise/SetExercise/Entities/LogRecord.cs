77e330a22e7fc9adbcb702d575ab286be2b168e2
André Luis dos Santos Sernajoto <69870279+andresernajoto@users.noreply.github.com>
2022-05-08 18:35:42 -0300

    Exercício resolvido
    Utilização das operações StreamReader, GetHashCode e Equals para identificação de quantos usuários acessaram um sistema.

---
+++ Intermediário/Exercícios/SetExercise/SetExercise/Entities/LogRecord.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SetExercise.Entities {
+    class LogRecord {
+
+        public string Username { get; set; }
+        public DateTime Instant { get; set; }
+
+        public LogRecord(string username, DateTime instant) {
+            Username = username;
+            Instant = instant;
+        }
+
+        // adquire o HashCode de Username
+        public override int GetHashCode() {
+            return Username.GetHashCode();
+        }
+
+        // verifica se há nomes iguais
+        public override bool Equals(object obj) {
+            if (!(obj is LogRecord)) {
+                return false;
+            }
+
+            // downcasting e retorno igualitário dos dados
+            LogRecord other = obj as LogRecord;
+            return Username.Equals(other.Username);
+        }
+    }
+}
---
+++ Intermediário/Exercícios/SetExercise/SetExercise/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using SetExercise.Entities;
+
+namespace SetExercise {
+    class Program {
+        static void Main(string[] args) {
+
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            // leitura do caminho do arquivo
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            // abre o arquivo, lê e imprime suas linhas
+            try {
+                using (StreamReader sr = File.OpenText(path)) {
+                    while (!sr.EndOfStream) {
+                        // leitura da linha e divisão do conteúdo em duas variáveis
+                        string[] line = sr.ReadLine().Split(' ');
+                        string username = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+
+                        // adição dos dados do usuário ao conjunto
+                        set.Add(new LogRecord(username, instant));
+                    }
+
+                    // inserção dos valores
+                    Console.WriteLine();
+                    Console.WriteLine("Total users: " + set.Count + " users founded!");
+                }
+                // caso haja alguma exceção, irá apresentá-la
+            } catch (IOException e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
