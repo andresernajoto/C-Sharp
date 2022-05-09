b8c472913ce60f1881cf5a4565e4206d7c5c87b8
André Luis dos Santos Sernajoto <69870279+andresernajoto@users.noreply.github.com>
2022-05-08 21:02:00 -0300

    Exercício resolvido
    Foram implementados o uso das operações GetHashCode e Equals para obter a quantidade de estudantes que um instrutor tem em 3 cursos diferentes, a partir do ID de seus alunos

---
+++ Intermediário/Exercícios/SetExercise2/SetExercise2/Entities/Students.cs
@@ -0,0 +1,27 @@
+﻿namespace SetExercise2.Entities {
+    class Students {
+        
+        // atributo da classe
+        public int StudentsID { get; set; }
+        
+        // construtor com o atributo
+        public Students(int studentsID) {
+            StudentsID = studentsID; 
+        }
+
+        // adquire o HashCode do ID do estudante
+        public override int GetHashCode() {
+            return StudentsID.GetHashCode();
+        }
+
+        // Obtém um booleano para o ID do estudante
+        public override bool Equals(object obj) {
+            if (!(obj is Students)) {
+                return false;
+            }
+
+            Students other = obj as Students;
+            return StudentsID.Equals(other.StudentsID);
+        }
+    }
+}
---
+++ Intermediário/Exercícios/SetExercise2/SetExercise2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using SetExercise2.Entities;
+
+namespace SetExercise2 {
+    class Program {
+        static void Main(string[] args) {
+
+            // instanciação de uma coleção Hash de Students
+            HashSet<Students> set = new HashSet<Students>();
+
+            // leitura da quantidade de estudantes no curso A
+            Console.Write("How many students for course A: ");
+            int num = int.Parse(Console.ReadLine());
+
+            // leitura do ID dos estudantes
+            for (int i = 0; i < num; i++) {
+                int numOfStudents = int.Parse(Console.ReadLine());
+                set.Add(new Students(numOfStudents));
+            }
+
+            // leitura da quantidade de estudantes no curso B
+            Console.Write("How many students for course B: ");
+            num = int.Parse(Console.ReadLine());
+
+            // leitura do ID dos estudantes
+            for (int i = 0; i < num; i++) {
+                int numOfStudents = int.Parse(Console.ReadLine());
+                set.Add(new Students(numOfStudents));
+            }
+
+            // leitura da quantidade de estudantes no curso C
+            Console.Write("How many students for course C: ");
+            num = int.Parse(Console.ReadLine());
+
+            // leitura do ID dos estudantes
+            for (int i = 0; i < num; i++) {
+                int numOfStudents = int.Parse(Console.ReadLine());
+                set.Add(new Students(numOfStudents));
+            }
+
+            // apresentação do resultado final
+            Console.WriteLine();
+            Console.WriteLine("Total of Students: " + set.Count + " students");
+        }
+    }
+}
