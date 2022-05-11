fd6be913a12253258c923372ca29bd3bbf3762c8
André Luis dos Santos Sernajoto <69870279+andresernajoto@users.noreply.github.com>
2022-05-10 23:22:42 -0300

    Conteúdo adicionado
    Primeiro exemplo de como realizar extensões dentro da linguagem.
    Exemplo com DateTime

---
+++ Intermediário/Aprendizado/ExtensionMethod/ExtensionMethod/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace System {
+    static class DateTimeExtensions {
+
+        public static string ElapsedTime(this DateTime thisObj) {
+            // subtrai o horário atual pelo passado como parâmetro
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+
+            // retorna a duração em horas e dias, dependendo do caso
+            if (duration.TotalHours < 24.0) {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " hours";
+            } else {
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " days";
+            }
+        }
+    }
+}
---
+++ Intermediário/Aprendizado/ExtensionMethod/ExtensionMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExtensionMethod {
+    class Program {
+        static void Main(string[] args) {
+
+            // instanciação um horário e apresentação do tempo decorrido
+            DateTime dt = new DateTime(2022, 05, 10, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
