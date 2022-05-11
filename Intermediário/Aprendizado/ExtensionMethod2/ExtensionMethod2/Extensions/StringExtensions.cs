da0f7bff3d16701e48861f735a4bdcab1a9b7948
André Luis dos Santos Sernajoto <69870279+andresernajoto@users.noreply.github.com>
2022-05-10 23:23:21 -0300

    Conteúdo adicionado
    Segundo exemplo de como realizar extensões dentro da linguagem.
    Exemplo com strings recebendo parâmetros.

---
+++ Intermediário/Aprendizado/ExtensionMethod2/ExtensionMethod2/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace System {
+    static class StringExtensions {
+
+        public static string Cut(this string thisObj, int count) {
+            // retornará a string cortada se passar do tamanho de count
+            if (thisObj.Length <= count) {
+                return thisObj;
+            } else {
+                return thisObj.Substring(0, count) + "...";
+            }
+        }
+    }
+}
---
+++ Intermediário/Aprendizado/ExtensionMethod2/ExtensionMethod2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExtensionMethod2 {
+    class Program {
+        static void Main(string[] args) {
+
+            // string recebendo um texto e aplicando o método de extensão
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
