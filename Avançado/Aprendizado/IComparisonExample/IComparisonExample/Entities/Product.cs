1d92ce1afac63c612bdc843730ba3e56052f7e89
André Luis dos Santos Sernajoto <69870279+andresernajoto@users.noreply.github.com>
2022-05-16 23:30:37 -0300

    Adicionado uma pasta de conteúdos
    Criada a pasta "Aprendizado" com o primeiro exercício mostrando um pouco sobre como funciona o tipo IComparision<T>.

---
+++ Avançado/Aprendizado/IComparisonExample/IComparisonExample/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace IComparisonExample.Entities {
+    class Product {
+
+        // declarçaão dos atributos
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        // construtor recebendo os atributos
+        public Product(string name, double price) {
+            Name = name;
+            Price = price;
+        }
+
+        // sobreescritura do ToString()
+        public override string ToString() {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Avançado/Aprendizado/IComparisonExample/IComparisonExample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using IComparisonExample.Entities;
+
+namespace IComparisonExample {
+    class Program {
+        static void Main(string[] args) {
+
+            // instanciação de uma lista do tipo Product
+            List<Product> products = new List<Product>();
+
+            // adição de produtos à lista
+            products.Add(new Product("TV", 900.00));
+            products.Add(new Product("Notebook", 1200.00));
+            products.Add(new Product("Tablet", 450.00));
+
+            // FORMA 1
+            /* organizando a lista por nome (por meio da função)
+            products.Sort(CompareProduct); */
+
+            // FORMA 2
+            /* Comparison<Product> comp = CompareProduct;
+            products.Sort(comp); */
+
+            // FORMA 3
+            // Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper();
+
+            // FORMA 4
+            // products.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+
+            // impressão de cada dado da lista
+            foreach (Product p in products) {
+                Console.WriteLine(products);
+            }
+        }
+
+        // FORMA 1
+        /* função que retorna a comparação entre produtos
+        static int CompareProduct(Product p1, Product p2) {
+            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+        } */
+        }
+    }
