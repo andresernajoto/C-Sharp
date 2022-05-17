5dd5a6c0b6142259ca90355969a719af09681f78
André Luis dos Santos Sernajoto <69870279+andresernajoto@users.noreply.github.com>
2022-05-16 23:49:13 -0300

    Conteúdo adicionado
    Exemplo da aplicação do método Delegate

---
+++ Avançado/Aprendizado/DelegateExample/DelegateExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DelegateExample.Services;
+
+namespace DelegateExample {
+
+    // função delegate que retorna dois valores double
+    delegate double BinaryNumericOperation(double n1, double n2);
+
+    class Program {
+        static void Main(string[] args) {
+
+            // valores
+            double a = 10;
+            double b = 12;
+
+            // aplicação das funções da classe CS para o delegate
+            BinaryNumericOperation op1 = CalculationService.Sum;
+            BinaryNumericOperation op2 = CalculationService.Max;
+
+            // apresentação do resultado
+            double result = op1(a, b);
+            Console.WriteLine("Soma dos valores: " + result);
+
+            // apresentação do resultado
+            result = op2(a, b);
+            Console.WriteLine("O maior valor encontrado é: " + result);
+        }
+    }
+}
---
+++ Avançado/Aprendizado/DelegateExample/DelegateExample/Services/CalculationService.cs
@@ -0,0 +1,19 @@
+﻿namespace DelegateExample.Services {
+    class CalculationService {
+
+        // retorna um valor máximo entre x e y
+        public static double Max(double x, double y) {
+            return (x > y) ? x : y;
+        }
+
+        // retorna a soma de x e y
+        public static double Sum(double x, double y) {
+            return x + y;
+        }
+
+        // retorna o dobro de x
+        public static double Square(double x) {
+            return x * x;
+        }
+    }
+}
