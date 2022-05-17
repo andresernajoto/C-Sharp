573088ae1d8bde0ec1851f592e2cdd9891117f60
André Luis dos Santos Sernajoto <69870279+andresernajoto@users.noreply.github.com>
2022-05-16 23:57:37 -0300

    Conteúdo adicionado
    Exemplo mostrando como funciona o Multicast delegate.

---
+++ Avançado/Aprendizado/MultiDelegates/MultiDelegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using MultiDelegates.Services;
+
+namespace MultiDelegates {
+
+    // declaração do delegate
+    delegate void BinaryNumericOperation(double n1, double n2);
+
+    class Program {
+        static void Main(string[] args) {
+
+            // valores
+            double a = 10;
+            double b = 12;
+
+            // chamada do delegate com multicast
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+
+            // apresentação dos métodos em ordem de chamada
+            op.Invoke(a, b);
+        }
+    }
+}
---
+++ Avançado/Aprendizado/MultiDelegates/MultiDelegates/Services/CalculationService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MultiDelegates.Services {
+    class CalculationService {
+
+        // método que mostra o valor máximo entre x e y
+        public static void ShowMax(double x, double y) {
+            double max = (x > y) ? x : y;
+            Console.WriteLine("O maior valor encontrado é: " + max);
+        }
+
+        // método que mostra a soma entre x e y
+        public static void ShowSum(double x, double y) {
+            double sum = x + y;
+            Console.WriteLine("A soma entre os valores é: " + sum);
+        }
+    }
+}
