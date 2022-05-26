195c411ed8d546541a6fd3fd42bb4dd46e8553b6
André Luis dos Santos Sernajoto <69870279+andresernajoto@users.noreply.github.com>
2022-05-26 00:57:10 -0300

    Conteúdo adicionado
    Implementando o delegate Predicate, que retorna um valor booleano.

---
+++ Avançado/Aprendizado/PredicateExample/PredicateExample/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace PredicateExample.Entities {
+    class Product {
+
+        // atributos da classe
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        // construtor com os atributos
+        public Product(string name, double price) {
+            Name = name;
+            Price = price;
+        }
+
+        // impressão padrão dos valores
+        public override string ToString() {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Avançado/Aprendizado/PredicateExample/PredicateExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using PredicateExample.Entities;
+
+namespace PredicateExample {
+    class Program {
+        static void Main(string[] args) {
+
+            // instanciação de uma lista de produtos
+            List<Product> list = new List<Product>();
+
+            // adição dos produtos à lista
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            // remoção dos produtos de acordo com a função
+            list.RemoveAll(ProductTest);
+
+            // impressão de cada produto adicionado à lista
+            foreach (Product p in list) {
+                Console.WriteLine(p);
+            }
+        }
+
+        // função que retorna somente os produtos maiores que 100
+        public static bool ProductTest(Product p) {
+            return p.Price >= 100;
+        }
+    }
+}
