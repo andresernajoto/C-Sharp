544d303f4555e6c40abe1458e89d75170e3ecca9
André Luis dos Santos Sernajoto <69870279+andresernajoto@users.noreply.github.com>
2022-05-26 00:58:11 -0300

    Conteúdo adicionado
    Utilizando um pouco de LINQ no código.

---
+++ Avançado/Aprendizado/LinqExample/LinqExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqExample {
+    class Program {
+        static void Main(string[] args) {
+
+            // specify data source
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            // define query expression
+            var result = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
+
+            // execute query
+            foreach (int x in result) {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
